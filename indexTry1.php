f184adad88356fb3d2a3840d11fbe42f62ddecc8
n-s-rudnytska <rudnitskayanataliastepanovna@gmail.com>
2021-06-11 10:13:15 +0300

    test

---
+++ indexTry1.php
@@ -0,0 +1,30 @@
+  <?php
+function test(){
+echo"<pre>";
+
+$res=file_get_contents('json.json');
+$data=json_decode($res,true);
+
+
+  echo "Пример функции.\n";
+  //echo"<br>";
+ // var_dump($data);
+ //print_r(array_chunk($data, 1));
+ for ($i=0; $i < count($data['contacts']); $i++) { 
+   # code...
+ 
+ $user=($data['contacts'][$i]['user'])-1;
+ $role=($data['users'][$user]['role'])-1;
+ $permission=($data['users'][$user]['permissions'])-1;
+ //print($data['contacts'][0]);
+ print($data['users'][$user]['name']);
+ print($data['roles'][$role]['roleName']);
+ print($data['contacts'][$i]['firstName']);
+ print($data['contacts'][$i]['lastName']);
+ print('-');
+ print($data['permissions'][$permission]['value']);
+ echo"<br>";
+}
+}
+test();
+   ?>
